cdfbda46b3c72f211f2931dfb9fa772c8ebad0e0
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-28 17:07:44 +0600

    modified server

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Mohosin I'm Running!");
 });
 
 export default router;
